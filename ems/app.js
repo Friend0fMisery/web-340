9001148cb376079160bb7d34babfd8776119b1fc
Justin Singleton <jsingleton@my365.bellevue.edu>
2019-09-24 19:59:18 -0500

    update for csrf

--- ems/app.js
+++ ems/app.js
@@ -15,8 +15,14 @@ var path = require('path');
 var mongoose = require('mongoose');
 var logger = require('morgan');
 var helmet = require('helmet');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var csrf = require('csurf');
 var Employee = require("./models/employee");
+
 var mongoDB = "mongodb+srv://Friend0fMisery:Kool6767!@buwebdev-cluster-1-sn3vf.mongodb.net/test?retryWrites=true&w=majority";
+var csrfProtection = csrf({cookie: true});
+
 var app = express();
 
 mongoose.connect(mongoDB, {
@@ -35,6 +41,17 @@ app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(logger('short'));
 app.use(helmet.xssFilter());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+app.use(csrfProtection);
+app.use(function(req, res, next) {
+  var token = req.csrfToken();
+  res.cookie('XSRF-TOKEN', token);
+  res.locals.csrfToken = token;
+  next();
+});
 
 app.get("/", function(req, res) {
   res.render("index", {
@@ -42,8 +59,22 @@ app.get("/", function(req, res) {
   });
 });
 
-app.get("/view", (req, res) => {
-  res.render("view");
+app.get("/view", function(req, res) {
+  if(!req.body.txtName) {
+    res.status(400).send('Entries must have a name');
+    return;
+  }
+  const employeeName = req.body.txtName;
+  console.log(employeeName);
+
+  res.render("view", {
+    title: "View"
+  });
+});
+
+app.post("/process", function(req, res) {
+  console.log(req.body.txtName);
+  res.redirect("/");
 });
 
 http.createServer(app).listen(8080, function() {
